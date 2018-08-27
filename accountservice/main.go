c6fc01efb5dce7e988a7be63fe165ea2e6e575dc
John <john@Johns-Mac.local>
2018-08-27 21:16:10 +0800

    added accountservice

---
+++ accountservice/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xqtech/goblog/accountservice/dbclient"
+
+	"github.com/xqtech/goblog/accountservice/service"
+)
+
+var appName = "accountservice"
+
+func main() {
+	fmt.Printf("Starting %v\n", appName)
+	initializeBoltClient()
+	service.StartWebServer("6767")
+}
+
+//Creates instance and calls the OpenBoltDb and Seed funcs
+func initializeBoltClient() {
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
+}
---
+++ accountservice/service/router.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+
+	//Create an instance of the Gorilla router
+	router := mux.NewRouter().StrictSlash(true)
+
+	//Iterate over the routes we declared in routes.go
+	//and attach them to the router instance
+	for _, route := range routes {
+		router.Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+	return router
+}
